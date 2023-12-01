ea7e9075dd1127ed2f724873480bb6ab8912f4d2
Mikael Nilsson <micke239@gmail.com>
2023-12-01 20:12:04 +0100

    first

---
+++ src/main/kotlin/Day2.kt
@@ -0,0 +1,18 @@
+import java.io.File
+
+
+class Day2{
+    fun run() {
+        val lines = File("src/main/resources/day2.txt").readLines()
+        part1(lines)
+        part2(lines)
+    }
+
+    private fun part1(lines: List<String>) {
+
+    }
+
+    private fun part2(lines: List<String>) {
+
+    }
+}
---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,6 @@
+fun main(args: Array<String>) {
+    when (args[0]) {
+        "1" -> Day1().run()
+        "2" -> Day2().run()
+    }
+}
