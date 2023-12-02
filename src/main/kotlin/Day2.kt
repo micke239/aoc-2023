56e43d3d8e276e4836e9b8e3cd76ad4cb4181be3
Mikael Nilsson <micke239@gmail.com>
2023-12-02 08:51:27 +0100

    second

--- src/main/kotlin/Day2.kt
+++ src/main/kotlin/Day2.kt
@@ -1,6 +1,5 @@
 import java.io.File
 
-
 class Day2{
     fun run() {
         val lines = File("src/main/resources/day2.txt").readLines()
@@ -9,10 +8,63 @@ class Day2{
     }
 
     private fun part1(lines: List<String>) {
+        val maxCubes = mapOf(
+            "red" to 12,
+            "green" to 13,
+            "blue" to 14
+        )
 
+        val sum = lines.map { line ->
+            val s1 = line.split(":")
+            val idContainer = s1[0]
+            val info = s1[1]
+
+            val id = idContainer.split(" ")[1].toInt()
+            val rounds = info.split("; ").map {
+                it.split(", ").associate { cube ->
+                    val pair = cube.trim().split(" ")
+                    pair[1] to pair[0].toInt()
+                }
+            }.toList()
+
+            Game(id, rounds)
+        }.filter {
+            !it.rounds.any { round -> round.any {cube -> cube.value > maxCubes[cube.key]!! }  }
+        }.sumOf { it.id }
+
+        println(sum)
     }
 
     private fun part2(lines: List<String>) {
+        val sum = lines.map { line ->
+            val s1 = line.split(":")
+            val idContainer = s1[0]
+            val info = s1[1]
 
+            val id = idContainer.split(" ")[1].toInt()
+            val rounds = info.split("; ").map {
+                it.split(", ").associate { cube ->
+                    val pair = cube.trim().split(" ")
+                    pair[1] to pair[0].toInt()
+                }
+            }.toList()
+
+            Game(id, rounds)
+        }.map {
+            it.rounds.fold(mutableMapOf<String, Int>()) { acc, round ->
+                round.forEach { cube ->
+                    if (cube.value > (acc[cube.key] ?: 0)) {
+                        acc[cube.key] = cube.value
+                    }
+                }
+                acc
+            }
+        }.sumOf {
+            it.entries.fold(1L) { acc, e -> acc * e.value }
+        }
+
+        println(sum)
     }
 }
+
+data class Game(val id: Int, val rounds: List<Map<String, Int>>)
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,5 +2,6 @@ fun main(args: Array<String>) {
     when (args[0]) {
         "1" -> Day1().run()
         "2" -> Day2().run()
+        "3" -> Day3().run()
     }
 }
